7451ff34cd749c8430ff0ec667620b2b98900f3a
Marcelo <marcelo.ungaretti@gmail.com>
2021-12-27 15:14:49 -0300

    fim do cap 5

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -4,10 +4,17 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Pessoas extends Model {
+
     static associate(models) {
-      // define association here
+      Pessoas.hasMany(models.Turmas, {
+        foreignKey: 'docente_id'
+      });
+      Pessoas.hasMany(models.Matriculas, {
+        foreignKey: 'estudante_id'
+      });
     }
   };
+  
   Pessoas.init({
     nome: DataTypes.STRING,
     ativo: DataTypes.BOOLEAN,
